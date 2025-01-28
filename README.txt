52ffd5c5e5fc89d30bf2358741b2b027969e79e7
Lev3005 <lev000666111@gmail.com>
2025-01-28 18:58:16 +0300

    second commit

--- README.txt
+++ README.txt
@@ -5,6 +5,9 @@
 Но самое главное здесь в Java Script, в будующем я планирую реализовать клон какого-нибудь интернет магазина.
 Пожелаем мне удачи!
 
+Измененно: Я добавил подсчет стоимости товаров в корзине,
+а так же перенес все данные карточек товаров в отдельный JSON файл
+
 Спасибо за просмотр данного проекта)
 
 
@@ -15,5 +18,8 @@ In this project I implemented a product counter and adding them to the cart.
 I am very proud of this project, although it does not look very presentable.
 But the most important thing here is in Java Script, in the future I plan to implement a clone of some online store.
 Let's wish me luck!
+
+Changed: I added a calculation of the cost of goods in the cart,
+All product card data is also transferred to a separate JSON file.
 
 Thanks for viewing this project)
--- js/main.js
+++ js/main.js
@@ -42,6 +42,14 @@ window.addEventListener("click", function(evnent) {
             toggleBacketStatus()
         }
     }
+
+    if (evnent.target.closest(".card__button")) {
+        backetPrice()
+    }
+
+    if (evnent.target.closest(".backet__card-button")) {
+        backetPrice()
+    }
 })
 
 // Корзина товаров
@@ -85,6 +93,7 @@ window.addEventListener("click", function(evnent) {
         card.querySelector('[class="card__button-counter"]').innerText = "1"
 
         toggleBacketStatus()
+        backetPrice()
     }
 })
 
@@ -99,4 +108,55 @@ function toggleBacketStatus() {
     } else {
         basketVoid.classList.remove("none")
     }
+}
+
+// Подсчет стоимости товаров
+
+function backetPrice() {
+
+    const cartPrivew = document.querySelectorAll(".backet__card-privew")
+    const allPriceView = document.querySelector(".backet__price-span")
+    let allSummaPrice = 0
+
+    cartPrivew.forEach( function(item) {
+
+        const itemPrice = item.querySelector(".backet__card-price")
+        const itemCounter = item.querySelector(".backet__button-counter")
+        const summaPrice = parseInt(itemPrice.innerText) * parseInt(itemCounter.innerText)
+        allSummaPrice += summaPrice
+    })
+
+    allPriceView.innerText = allSummaPrice
+}
+
+// JSON товары
+
+const productJSON = document.querySelector(".cards")
+
+productJson()
+
+async function productJson() {
+
+    const response = await fetch("./js/products.json") 
+    const productsArray = await response.json()
+
+    renderProducts(productsArray)
+}
+
+function renderProducts(productsArray) {
+
+    productsArray.forEach(function(item) {
+        const productHTML = `<div class="card" id="${item.id}">
+                                <img src="${item.img}" alt="Чизбургер" class="card__img">
+                                <h2 class="card__title">${item.title}</h2>
+                                <p class="card__price">${item.price}р</p>
+                                <div class="card__button">
+                                    <button class="card__button-one">-</button>
+                                    <p class="card__button-counter">1</p>
+                                    <button class="card__button-two">+</button>
+                                </div>
+                                <button class="card__backet">ДОБАВИТЬ В КОРЗИНУ</button>
+                            </div>`
+        productJSON.insertAdjacentHTML("beforeend", productHTML)
+    })
 }
